82f1ba42d82f5cea814ca96c798940658f9eb82a
Nathan Love <ngl9789@gmail.com>
2025-09-26 18:42:36 +0000

    update devfile

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.PathParam;
 
 @Path("/hello")
 public class GreetingResource {
@@ -12,5 +13,12 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{name}")
+    public String helloName(@PathParam("name") String name) {
+        return "Hello " + name;
     }
 }
